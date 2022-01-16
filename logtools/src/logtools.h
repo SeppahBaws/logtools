054945cf3480b2ccc3cdcb2837de74268a3c5126
Seppe Dekeyser <seppe@dva.be>
2022-01-16 02:34:43 +0100

    Happy new year!

--- logtools/src/logtools.h
+++ logtools/src/logtools.h
@@ -5,7 +5,7 @@
  * -----------------------------[   LICENSE   ]-----------------------------
    MIT License
    
-   Copyright (c) 2020-2021 Seppe Dekeyser
+   Copyright (c) 2020-2022 Seppe Dekeyser
    
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
